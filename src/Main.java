503a61f4ac9b4b57c58903f48a3e4c80f62968b9
Edinichkin <11th@bk.ru>
2022-10-11 20:02:37 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            if (Data.check("123", "_", "_")) {
+                System.out.println("Данные корректны");
+            }
+        } catch (WrongLoginException | WrongPasswordException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
